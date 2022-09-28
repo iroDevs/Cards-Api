9d78ba7d48ca2d0a3c42c7273b36a7c24f9b7b84
unknown <pedrohenriquelouresoliveira@gmail.com>
2022-09-28 00:40:22 -0300

    salvando setup

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+
+class App {
+    public expres: express.Application 
+
+    public constructor() {
+        dotenv.config()
+        this.expres = express()
+        this.expres.use(express.json())
+        this.expres.use(cors())
+}
+    private middleware (): void {
+        console.log("entrou no middleware");
+        
+    }
+
+    private database (): void {
+        mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@localhost:27017`)
+    }
+}
