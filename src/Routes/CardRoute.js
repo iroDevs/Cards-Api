649d87638c3c26df3664cd29c57b848c8b083b29
unknown <pedrohenriquelouresoliveira@gmail.com>
2022-09-28 03:04:30 -0300

    create cards

--- src/Routes/CardRoute.js
+++ src/Routes/CardRoute.js
@@ -5,5 +5,6 @@ import CardController from '../controller/CardController'
 const routes = Router()
 
 routes.get('/cards',CardController.returnAll )
+routes.post('/cards',CardController.createCard)
 
 export default routes
--- src/controller/CardController.ts
+++ src/controller/CardController.ts
@@ -1,8 +1,15 @@
 import { Request , Response } from "express"
+import Card from "../schemas/Card"
 
 class CardController {
     public async returnAll ( req: Request, res:Response) : Promise<Response>{
-        return  res.send("retorno")
+       const cards = await Card.find()
+       return res.status(200).json(cards);
+    }
+
+    public async createCard (req: Request, res:Response) : Promise<Response>{
+        const card = await Card.create(req.body);
+        return res.status(201).json(card);
     }
 }
 
