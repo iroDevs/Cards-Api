f29926eb3e5ad738e392169e564e957f49e042cd
unknown <pedrohenriquelouresoliveira@gmail.com>
2022-09-28 19:36:29 -0300

    criando build

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,8 @@
-const number  = 8;
-console.log(number);
+import app from './app'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.NODE_PORT || '3999';
+
+app.listen(PORT);
--- src/app.ts
+++ src/app.ts
@@ -29,9 +29,7 @@ class App {
     private database (): void {      
         mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@localhost:27017`)
         
-        //mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=writeapp&w=1
-        //mongodb://${process.env.MONGO_USER}:{process.env.MONGO_PASSWORD}@localhost:27017/CardDB
-        //mongodb://iroDev:flamengo123@localhost:27017/
+   
     }
 
     private routes (): void {
